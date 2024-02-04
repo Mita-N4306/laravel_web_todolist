7a6a5f0022cd7df72ffa6667d9ccd0c554b5f6b5
naoki mitamura <zango3333@yahoo.co.jp>
2024-02-04 18:51:23 +0900

    トップページの作成修正(2024.2.4)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,10 +9,9 @@
   </head>
   <body>
     <div class="content">
-     @include('commons.error_messages')
-     @yield('content')
+      @include('commons.error_messages')
+      @yield('content')
     </div>
-    </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </body>
 </html>
